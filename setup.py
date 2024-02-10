4c1b3b695fe8860eaf16960ba61749437838d762
greydoubt <43443470+greydoubt@users.noreply.github.com>
2024-02-10 03:12:44 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="YourProjectName",
-    version="0.1.0",
+    name="m77",
+    version="0.0.0.1",
+    author="greydoubt",
+    author_email="your@email.com",
+    description="...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/greydoubt/m77",
     packages=find_packages(),
-    # Add other parameters as needed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
