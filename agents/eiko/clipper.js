90cc1a54cd801c97b1fd4c85a03746813ef24a58
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-06-14 07:59:25 -0700

    Create clipper.js

---
+++ agents/eiko/clipper.js
@@ -0,0 +1,3 @@
+<script id="aampli_plg_scr_configure-js-extra">
+var aampliPlgScrPayload = {"apiKey":"75427f25d4841735a1427c2121894372","sessionReplay":"1","sampleRate":"10","shouldChangeLibrary":"1","isEuServerZone":"0"};
+</script>
