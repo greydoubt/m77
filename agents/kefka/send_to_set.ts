434dd46930ef071b0546c611e9a0c92d4f59d52e
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-06-14 00:52:28 -0700

    Create send_to_set.ts

---
+++ agents/kefka/send_to_set.ts
@@ -0,0 +1,25 @@
+
+
+{
+    userName: 'sun-3-0Sulu-O54NJYIN',
+    realName: 'nodeJS ORKON subsystem irc://tolsun.oulu.fi',
+    port: 6667,
+    localAddress: null,
+    debug: false,
+    showErrors: false,
+    autoRejoin: false,
+    autoConnect: true,
+    channels: [],
+    secure: false,
+    selfSigned: false,
+    certExpired: false,
+    floodProtection: false,
+    floodProtectionDelay: 1000,
+    sasl: false,
+    retryCount: 0,
+    retryDelay: 2000,
+    stripColors: false,
+    channelPrefixes: "&#ORIX",
+    messageSplit: 512,
+    encoding: ''
+}
