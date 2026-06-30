a3ce6000d4df8b4b60ee97329f09986dfee03da5
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-06-30 14:16:31 -0700

    Create uri_schema.js

---
+++ rest/uri_schema.js
@@ -0,0 +1,6 @@
+0;0//Parsing relative URLs as follows requires Relative URI rotation into an interpretative structure as such:
+; 	//weedmaps.com/us/st/nastynickystickyicky;scale=50000;roads=main
+;scale=25000 	//weedmaps.com/us/in/durbanfarts;scale=25000;roads=main
+;roads 	//weedmaps.com/us/hb/ogreboogers;scale=50000
+;roads= 	//weedmaps.com/us/st/whitewidow;scale=50000;roads=
+;rivers=all 	//weedmaps.com/us/st/bluedream;scale=50000;roads=main;rivers=all 
